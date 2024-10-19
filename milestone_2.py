234fc52e1a829242e285df7b2ce8ce3b5bb385c9
parul118 <67359019+parul118@users.noreply.github.com>
2024-10-19 16:00:28 +0100

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,8 @@
 word_list = ['Apple','Banana','Mango','Guava','Grapes']
 print('Favourite Fruits',word_list)
+guess = input("enter a single letter")
+if (( len(guess) == 1 ):
+  if(isalpha(guess)):
+    print( "Good guess!")
+  else : 
+    print("Oops! That is not a valid input.")
