31154057b468d487d0e31b147df4eb3149999d9e
parul118 <67359019+parul118@users.noreply.github.com>
2024-10-19 16:33:58 +0100

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -6,3 +6,9 @@ while True:
             break
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
+
+secret_word = "apple"
+if guess in secret_word:
+    print("Good guess!" + guess + " is in the word.")
+else:
+    print("Sorry," +  guess + " is not in the word. Try again")
