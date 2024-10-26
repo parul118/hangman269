c1a280dc842c1d7d7b1e3432bb514df5b889636d
parul118 <67359019+parul118@users.noreply.github.com>
2024-10-26 18:26:02 +0100

    Update milestone_5.py

--- milestone_5.py
+++ milestone_5.py
@@ -25,25 +25,25 @@ class Hangman:
         print(self.word_guessed)
          
 
-    def check_guess (self,word , guess , word_guessed , num_letters):
+    def check_guess (self, guess):
         guess = guess.lower()
-        if guess in word:
+        if guess in self.word:
             print("Good guess!" + guess + " is in the word.")
-            for i in range(len(word)):                                                                                                                                                                         
-                if (word[i] == guess):
-                    word_guessed[i] = guess
+            for i in range(len(self.word)):                                                                                                                                                                         
+                if (self.word[i] == guess):
+                    self.word_guessed[i] = guess
                     
-            return num_letters - 1
+            self.num_letters-1
         else:
             self.num_lives = self.num_lives - 1
             
             print("Sorry," +  guess + " is not in the word. Try again")
-            print("You have " + str(self.num_lives) + " left.")
+            print("You have " + str(self.num_lives) + "  lives left.")
 
         
     def ask_for_input(self):
         
-        while True:
+        while self.num_lives > 0:
         
             guess = input("guess the letter")
             if ( len(guess) != 1 ):
@@ -52,31 +52,25 @@ class Hangman:
             elif ( guess in self.list_of_guesses):
                 print("You already tried that letter!")
             else:
-                self.check_guess(self.word ,guess ,self.word_guessed,self.num_letters)
+                self.check_guess( guess )
                 self.list_of_guesses.append(guess)
-                
-
-
-def play_game(word_list):
-    num_lives = 5
-    
-    game = Hangman(word_list,num_lives)
-
-    while True:
-        
-        if game.num_lives == 0:
-            print('You lost!')
-            break
-        
-        elif game.num_letters > 0:
-            
-            game.ask_for_input()
-            
-        if gam.num_lives > 0:
-            if game.num_letters <=0:
-                
-                print('Congratulations. You won the game!')
                 break
+                
+
+    @staticmethod
+    def play_game(word_list):
+        
+    
+        game = Hangman(word_list)
+        while game.num_lives > 0 and game.num_letters > 0:
+            game.ask_for_input()
+            print(" ".join(game.word_guessed))
+            if game.num_letters == 0:
+                print("Congratulations! You won the game!")
+                break
+            elif game.num_lives == 0:
+                print("You lost! The word was:", game.word)
+                
             
 word_list = ['python', 'java', 'kotlin', 'javaScript']
-play_game(word_list)
+game = Hangman.play_game(word_list)
